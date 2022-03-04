3a8d28ee0e15907b797599a64c557a92d5b6e61d
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2022-03-04 11:37:55 -0600

    JavaDoc and Junit test case added.

---
+++ src/test/java/edu/banditutorials/beveragebar/MilkTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package edu.banditutorials.beveragebar;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Anil Kumar Kolla(S545232@nwmissouri.edu)
+ */
+public class MilkTest {
+    
+    public MilkTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDescription method, of class Milk.
+     */
+    @Test
+    public void testGetDescription() {
+        System.out.println("getDescription");
+        Milk instance = null;
+        String expResult = "";
+        String result = instance.getDescription();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
